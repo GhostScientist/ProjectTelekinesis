dbb9e2f1d730ae89bfe4085aa84d58eb674ecab2
Dakota Kim <codingvampire@gmail.com>
2025-04-07 20:59:09 -0400

    add ios project

---
+++ ProjectTelekinesis/ProjectTelekinesis/ProjectTelekinesisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectTelekinesisApp.swift
+//  ProjectTelekinesis
+//
+//  Created by Dakota Kim on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ProjectTelekinesisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
